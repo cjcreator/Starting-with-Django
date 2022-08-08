5684407c84e9d6854a208c630440808325a04a16
cjcreator <103061808+cjcreator@users.noreply.github.com>
2022-08-08 06:26:35 +0530

    Update django_install.py

--- django_install.py
+++ django_install.py
@@ -1,17 +1,32 @@
-GNU nano 6.3         django_install.py
-pip install virtualenv
 
+pip install virtualenv
+#firstly install the virtual environment
 pwd
+#create a new directory by using command - mkdir <filename>
+
 mkdir django
-cd django
-ls
+cd django #openit
+#ls ---listing for linux users
+#Dir --listing for windows users
+
 #creating virtual envirnment
 virtualenv trydjango
 
 cd trydjango
-pip install Django
-ls
-source bin/activate
+pip install Django 
+#ls ---listing for linux users
+#Dir --listing for windows users
+#methods to activate virtual environment:
+source bin/activate 
+#activate👆👆 the virtual environment by using command for linux user
+#note:-if you are windows users then type 👇👇
+./Scripts/activate.bat
+#   Or
+cd scripts
+Activate.bat
+
+
+
 mkdir src #create a directory src
 ls
 cd src
